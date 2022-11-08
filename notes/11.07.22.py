ab0f304b5104547806d48e8061d60815a7f3f5e1
Aguillon Designs <leo.aguillondesigns@gmail.com>
2022-11-07 20:24:49 -0600

    update notes for 11.07

--- notes/11.07.22.py
+++ notes/11.07.22.py
@@ -1,6 +1,13 @@
+# Rounding
 num1 = 105.65
 num2 = 99.3524816
 print(round(num1, 0))
 print(round(num2, 2))
 
 #Try/catch
+number = "float"
+try:
+    asNumber = int(number)
+    print(asNumber)
+except:
+    print("unable to convert " + number + " to an integer")
