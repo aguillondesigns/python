ab4a38fa1f1136b9adfe0c8505245d4e3c21abbb
Aguillon Designs <leo.aguillondesigns@gmail.com>
2022-11-08 20:54:50 -0600

    saving property assignment

---
+++ assignments/propertychange.py
@@ -0,0 +1,19 @@
+# Assignment
+# Display 3 Properties
+# Allow the user to change one of the properties using the key
+# System will generate a random property and display all 3 properties again
+
+'''  Initial properties will be randomly generated
+1 Strength 210
+2 Armor 156
+3 Resistance 95
+
+Which property would you like to change? 
+1
+
+1 Life Steal 5%
+2 Armor 156
+3 Resistance 95
+
+Loop until user is happy
+'''
